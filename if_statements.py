c662d10b86a4050a9d8d687e5c0c30b3c6a4251e
Christian Sorto <christiansrto@gmail.com>
2025-05-20 16:27:03 -0400

    Logical opperators and Conditional expressions

--- if_statements.py
+++ if_statements.py
@@ -20,3 +20,30 @@ if for_sale:
     print("The item is for sale.")
 else:
     print("The item is not for sale.")
+
+
+#Logical operators = and, or, not 
+#They are used to combine conditional statements
+
+temp = 30
+is_raining = False
+
+if temp > 25 and not is_raining:
+    print("It's a nice day for a walk.")
+elif temp <0 and is_raining:
+    print("Be careful, it's icy outside!")
+elif temp < 0 or is_raining:
+    print("Maybe stay indoors.")
+
+
+
+#Conditional expressions = A one-liner if statement also known as a ternary operator
+
+num = 5 
+x = 10
+y = 20
+
+print ("Even" if num % 2 == 0 else "Odd")
+
+max_value = x if x > y else y
+print(f"The maximum value is: {max_value}")
