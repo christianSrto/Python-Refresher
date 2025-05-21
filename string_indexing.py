540f4630ed3cc99bb30662638758a1640a1c90c2
Christian Sorto <christiansrto@gmail.com>
2025-05-20 21:11:31 -0400

    string indexing

---
+++ string_indexing.py
@@ -0,0 +1,15 @@
+# Indexing = accessing elements of a sequence using [] 
+# [Start:Stop:Step]
+
+phone_number = "123-456-7890"
+print(phone_number[0])  
+print(phone_number[:3])  
+print(phone_number[4:7])
+print(phone_number[8:])
+print(phone_number[-1])
+
+#Prints every character per step
+print(phone_number[::2])
+
+# Reverses the string
+print(phone_number[::-1])  
