fcda937a9d252d70071c54044a145aa213fd723e
Christian Sorto <christiansrto@gmail.com>
2025-05-22 23:00:05 -0400

    for loops

---
+++ for_loops.py
@@ -0,0 +1,26 @@
+#For Loops = execute a block of code a fixed number of times
+
+for counter in range(1, 11):
+    print(counter)
+
+for counter in reversed(range(1,11, 2)):
+    print(counter)
+
+credit_card = "1234-5678-9012-3456"
+
+for x in credit_card:
+    print(x)
+
+for x in range(1,21):
+    #skips the number 13 by using continue
+    if x == 13: 
+        continue
+    else:
+        print(x)
+
+for x in range(1,21):
+    #stops at 13
+    if x == 13: 
+        break
+    else:
+        print(x)
