245edacb04869814e815eca54d3b2b1470264b64
Christian Sorto <christiansrto@gmail.com>
2025-05-23 20:54:49 -0400

    nested loops

--- for_loops.py
+++ for_loops.py
@@ -24,3 +24,11 @@ for x in range(1,21):
         break
     else:
         print(x)
+
+
+#Nested Loops = A loop within another loop 
+
+for x in range(3):
+    for y in range (0,10):
+        print(y, end="")
+    print()
