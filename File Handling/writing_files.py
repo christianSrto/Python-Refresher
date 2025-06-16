6673ecb2ab0a565fe79ec65c2da0bf8a3934dad7
Christian Sorto <christiansrto@gmail.com>
2025-06-15 22:36:30 -0400

    writing txt and json files

---
+++ File Handling/writing_files.py
@@ -0,0 +1,57 @@
+# Python writing files (.txt, .json, .csv)
+
+import json
+import csv
+
+employees = ["Mr. Krabs", "Spongebob", "Squidward"]
+
+txt_data = "This works"
+
+file_path = "File Handling/output.txt"
+
+#other modes r = read, a = append, x = writes if that file doesn't exist
+with open(file=file_path, mode="w") as file:
+    for employee in employees:
+        file.write(employee + "\n")
+    print("file was created")
+
+try:
+    with open(file=file_path, mode="a") as file:
+        file.write("\n" + txt_data)
+        print("file was created")
+except FileExistsError:
+    print("that file already exists")
+
+try:
+    with open(file=file_path, mode="x") as file:
+        file.write("using x mode")
+        print("file was created")
+except FileExistsError:
+    print("that file already exists")
+
+
+
+#json file handling
+
+json_path = "File Handling/output.json"
+
+employee = {
+    "name": "Spongebob",
+    "age": 30,
+    "job": "cook"
+}
+
+with open(file=json_path, mode="w") as file:
+    json.dump(employee, file, indent=4)
+    print("json file was created")
+
+
+
+#csv file handling 
+
+employees = [["Name", "Age", "Job"],
+             ["Spongebob", 30, "Cook"],
+             ["Mr. Krabs", 50, "Owner"],
+             ["Squidward", 40, "Cashier"]]
+
+csv_path = "File Handling/output.csv"
